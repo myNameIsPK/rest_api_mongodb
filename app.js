5ebeeda1048497a22c3099ff788208456d7d667b
root <root@PK.localdomain>
2020-07-24 01:09:25 +0700

    fix npm start and fotmat document for prettier.

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-if(process.env.NODE_ENV !== 'production'){ // if app not run in production env then use dotenv.
+if (process.env.NODE_ENV !== 'production') { // if app not run in production env then use dotenv.
     require('dotenv').config(); // parse string in .env file into object.
 }
 
@@ -8,8 +8,8 @@ const expressLayouts = require('express-ejs-layouts'); //Layout support for ejs 
 const bodyParser = require('body-parser'); //Middleware for express to easily parse body from post request.
 const methodOverride = require('method-override'); // html form can use method DELETE and PUT.
 
-app.set('view engine','ejs'); // set express to use ejs in express settings.
-app.set('views', __dirname+'/views'); // set path for views.
+app.set('view engine', 'ejs'); // set express to use ejs in express settings.
+app.set('views', __dirname + '/views'); // set path for views.
 app.set('layout', 'layouts/layout.ejs'); // set layout of our website.
 app.use(expressLayouts); // set express to use expressLayouts.
 app.use(express.static('/public')); //set path for static files like style, img, css .
@@ -17,21 +17,21 @@ app.use(bodyParser.urlencoded({ limit: '10mb', extended: false })); //set expres
 app.use(methodOverride('_method')); // query <form action = '/any?_method=DELETE/PUT >
 
 const mongoose = require('mongoose');
-mongoose.connect(process.env.MONGODB_URL,{ 
-    useNewUrlParser: true, 
-    useUnifiedTopology: true, 
+mongoose.connect('mongodb+srv://dbUser:bnyQzJNmSUR9DVSd@cluster0.r8pua.mongodb.net/dbUser?retryWrites=true&w=majority', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
     useFindAndModify: false
-}).catch(err => 
-    console.error('Initial connection to mongoDB is ERROR ;( '+err) // handle initial connection error.
+}).catch(err =>
+    console.error('Initial connection to mongoDB is ERROR ;( ' + err) // handle initial connection error.
 );
 
 const db = mongoose.connection;
-db.on('error', err => console.error('ERROR after Initial connection ;( '+err) ); // handle error after initial connection. 
+db.on('error', err => console.error('ERROR after Initial connection ;( ' + err)); // handle error after initial connection. 
 db.once('open', () => console.log('Connected to mongoDB :)')); // when connection success.
 
 const indexRouter = require('./routes/index');
 const todosRouter = require('./routes/todos');
-app.use('/',indexRouter); // use controller(routes).
-app.use('/todos',todosRouter); 
+app.use('/', indexRouter); // use controller(routes).
+app.use('/todos', todosRouter);
 
-app.listen(process.env.PORT || 3000,()=>{console.log("Listening to 3000");});
+app.listen(process.env.PORT || 3000, () => { console.log("Listening to 3000"); });
--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 
 //GET
-router.get("/",(req,res)=>{
+router.get("/", (req, res) => {
     res.render('index');
 });
 
